a2e00c330ea239d26750f7a1514f18be61f41708
Luan Santos <git@luan.sh>
2023-06-10 10:19:49 -0700

    feat: split build and running the site

--- start.sh
+++ start.sh
@@ -1,9 +1,3 @@
 #!/bin/bash
 
-curl -fsSL https://get.pnpm.io/install.sh | sh -
-pnpm install --frozen-lockfile
-pnpm dlx prisma migrate deploy
-pnpm dlx prisma generate
-pnpm build
-export NODE_ENV=production
 node -r dotenv/config build
---
+++ update.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+curl -fsSL https://get.pnpm.io/install.sh | sh -
+pnpm install --frozen-lockfile
+pnpm dlx prisma migrate deploy
+pnpm dlx prisma generate
+pnpm build
