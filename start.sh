5b0cec971b8875e62187d3beb1e93af5423034cb
Luan Santos <git@luan.sh>
2023-06-10 09:19:31 -0700

    fix: run production in production

--- start.sh
+++ start.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
+export NODE_ENV=production
 curl -fsSL https://get.pnpm.io/install.sh | sh -
 pnpm install --frozen-lockfile
 pnpm dlx prisma migrate deploy
+pnpm dlx prisma generate
 pnpm build
 node -r dotenv/config build
