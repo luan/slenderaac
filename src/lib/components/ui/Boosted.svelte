70e79aedf3ec4d8442503d53a269dcf847e8c4d0
Luan Santos <git@luan.sh>
2023-06-13 19:10:32 -0700

    ui: nicer platform for boosted

--- src/lib/components/ui/Boosted.svelte
+++ src/lib/components/ui/Boosted.svelte
@@ -1,4 +1,6 @@
 <script lang="ts">
+	import { faBolt, faSkull } from '@fortawesome/free-solid-svg-icons';
+	import Fa from 'svelte-fa';
 	import { tooltip } from 'svooltip';
 
 	import type { BoostedProps } from '$lib/boosted';
@@ -11,17 +13,33 @@
 
 {#if boosted}
 	<div
-		class="transition-all ease-in-out duration-300 hover:scale-110"
+		class="transition-all ease-in-out duration-300 hover:scale-110 bg-surface-backdrop-token p-2 border-2 border-warning-500 ring-2 ring-primary-600/50 rounded-token shadow-md relative"
 		use:tooltip={{
 			content: `Boosted ${toProperCase(kind)}: ${boosted.boostname ?? ''}`,
 			offset: 20,
 		}}>
+		<div class="absolute inset-0 flex items-center justify-center">
+			<Fa
+				class="text-surface-900-50-token opacity-50"
+				icon={kind === 'boss' ? faSkull : faBolt}
+				size="32" />
+		</div>
 		<AnimatedOutfit
 			outfit={boosted}
 			alt={boosted.boostname ?? ''}
 			class="scale-110">
 			<div
-				class="w-full h-full bg-surface-800-100-token opacity-75 rounded-full blur-md" />
+				class="w-full h-full bg-surface-700-200-token opacity-40 rounded-full" />
 		</AnimatedOutfit>
+		<div
+			class="absolute inset-x-0 -bottom-4 flex items-center justify-center h-8">
+			<span
+				class="rounded-full bg-warning-500 ring-2 ring-primary-600/50 p-0.5">
+				<Fa
+					class="text-success-500"
+					icon={kind === 'boss' ? faSkull : faBolt}
+					size="12" />
+			</span>
+		</div>
 	</div>
 {/if}
--- src/lib/components/ui/BoostedSection.svelte
+++ src/lib/components/ui/BoostedSection.svelte
@@ -1,8 +1,4 @@
 <script lang="ts">
-	import { faRocket } from '@fortawesome/free-solid-svg-icons';
-	import Fa from 'svelte-fa';
-	import { _ } from 'svelte-i18n';
-
 	import type { BoostedProps } from '$lib/boosted';
 	import Boosted from '$lib/components/ui/Boosted.svelte';
 
@@ -11,11 +7,6 @@
 </script>
 
 <div class="flex flex-col-reverse items-center gap-2 mt-4 md:mt-0 md:-mb-4">
-	<span
-		class="hidden md:flex chip rounded-full variant-filled-tertiary opacity-75 mr-2 gap-2">
-		<Fa icon={faRocket} />
-		{$_('boosted')}
-	</span>
 	<div class="flex flex-row gap-8">
 		<Boosted boosted={boostedCreature} kind="creature" />
 		<Boosted boosted={boostedBoss} kind="boss" />
