270a39e5a2f2aced3525f5adcdcae88e267901f9
Luan Santos <git@luan.sh>
2023-06-16 18:36:57 -0700

    feat: better modals

--- src/lib/components/ui/StatelessModal.svelte
+++ src/lib/components/ui/StatelessModal.svelte
@@ -2,7 +2,8 @@
 	import { faClose } from '@fortawesome/free-solid-svg-icons';
 	import { Toast } from '@skeletonlabs/skeleton';
 	import { createEventDispatcher } from 'svelte';
-	import { fade } from 'svelte/transition';
+	import { fade, scale } from 'svelte/transition';
+	import { Body } from 'svelte-body';
 	import Fa from 'svelte-fa';
 	import { portal } from 'svelte-portal';
 
@@ -17,27 +18,31 @@
 	export let title: string;
 </script>
 
-<div
-	class="modal-backdrop fixed inset-0 bg-surface-backdrop-token"
+<Body class="overflow-hidden" />
+
+<dialog
+	class="modal-backdrop fixed inset-0 bg-surface-backdrop-token w-full h-full flex items-center justify-center"
+	on:click|self={close}
+	on:keypress={(e) => e.key === 'Escape' && close()}
+	transition:fade
 	use:portal={'body'}>
 	{#if browser}
 		<Toast />
 	{/if}
 
 	<div
-		class="w-full h-full p-4 overflow-y-auto flex justify-center items-center"
-		transition:fade={{ duration: 300 }}>
-		<div
-			class="modal block bg-surface-100-800-token w-modal h-auto p-4 space-y-4 rounded-container-token shadow-xl"
-			role="dialog"
-			aria-modal="true"
-			aria-label={title}>
-			<header
-				class="text-2xl font-bold flex flex-row justify-between items-center">
-				{title}
-				<button on:click={close}><Fa icon={faClose} size="xs" /></button>
-			</header>
+		class="modal flex flex-col bg-surface-100-800-token w-modal h-auto max-h-full overflow-hidden p-4 space-y-4 rounded-container-token shadow-xl"
+		role="dialog"
+		transition:scale
+		aria-modal="true"
+		aria-label={title}>
+		<header
+			class="text-2xl font-bold flex flex-row justify-between items-center">
+			{title}
+			<button on:click={close}><Fa icon={faClose} size="xs" /></button>
+		</header>
+		<div class="flex flex-col gap-1 h-auto overflow-y-auto">
 			<slot />
 		</div>
 	</div>
-</div>
+</dialog>
