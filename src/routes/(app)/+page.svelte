417d26f8d6b1fd43c3a808c6af25950227a53ef8
Luan <github@luan.sh>
2023-06-19 21:54:58 -0700

    ui: full width news

--- src/routes/(app)/+page.svelte
+++ src/routes/(app)/+page.svelte
@@ -11,26 +11,28 @@
 	export let data: PageData;
 </script>
 
-{#each data.articles as article, i}
-	<header
-		class="card !variant-filled-secondary p-1 px-4 text-md -mx-2 flex flex-row gap-2 items-center justify-between">
-		<span class="flex flex-row gap-2 items-center">
-			<Fa icon={faCalendar} size="xs" />
-			<span class="text-sm text-secondary-50"
-				>{formatDate(article.created_at)}</span>
-			<strong>{article.title}</strong>
-		</span>
-		<em class="text-sm">
-			{$_('published-by')}
-			<a href="/characters/{article.author.name}" class="text-secondary-200">
-				{article.author.name}
-			</a>
-		</em>
-	</header>
+<div class="w-full">
+	{#each data.articles as article, i}
+		<header
+			class="card !variant-filled-secondary p-1 px-4 text-md -mx-2 flex flex-row gap-2 items-center justify-between">
+			<span class="flex flex-row gap-2 items-center">
+				<Fa icon={faCalendar} size="xs" />
+				<span class="text-sm text-secondary-50"
+					>{formatDate(article.created_at)}</span>
+				<strong>{article.title}</strong>
+			</span>
+			<em class="text-sm">
+				{$_('published-by')}
+				<a href="/characters/{article.author.name}" class="text-secondary-200">
+					{article.author.name}
+				</a>
+			</em>
+		</header>
 
-	<Markdoc content={article.content} />
+		<Markdoc content={article.content} />
 
-	{#if i < data.articles.length - 1}
-		<hr class="divider" />
-	{/if}
-{/each}
+		{#if i < data.articles.length - 1}
+			<hr class="divider" />
+		{/if}
+	{/each}
+</div>
