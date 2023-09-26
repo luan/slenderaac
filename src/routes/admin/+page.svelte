d67a3ff530765632c9b1b97fc69397b78eb11cf2
Luan Santos <git@luan.sh>
2023-09-25 17:48:41 -0700

    fix: admin panel

--- src/routes/admin/+page.svelte
+++ src/routes/admin/+page.svelte
@@ -2,4 +2,7 @@
 </script>
 
 <h1 class="h1">Hello Admin</h1>
-<p>Welcome to the Slaunder Admin Panel. In this interface, you can manage News, the Library, and various static pages.</p>
+<p>
+	Welcome to the Slaunder Admin Panel. In this interface, you can manage News,
+	the Library, and various static pages.
+</p>
